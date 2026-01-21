915f4748468a7c23b76c682115f4a7bb56251a01
Nnamdi Amaga <nnamdiamaga2k20@gmail.com>
2026-01-21 11:41:06 +0100

    new meta keywords

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
     
     return (
         <footer className="w-full py-8 text-center text-slate-400 text-sm pb-24 md:pb-8">
-            <p>© {currentYear} <span className="font-mono font-bold text-indigo-500">NA.</span>.
+            <p>© {currentYear} <span className="font-mono font-bold text-indigo-500">NA</span>.
             </p>
         </footer>
     );
